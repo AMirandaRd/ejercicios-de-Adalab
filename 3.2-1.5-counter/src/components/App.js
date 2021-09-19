0b5523c7c4a7c7747a6d3232979c7759c4fc459e
Amirandard <am.miranda.rd@gmail.com>
2021-09-19 11:47:16 +0200

    add react files

---
+++ 3.2-1.5-counter/src/components/App.js
@@ -0,0 +1,15 @@
+
+import '../styles/App.css';
+
+function App() {
+  return (
+   
+      <header>
+        <h1> Sandra y Angela molan</h1>
+      </header>
+        
+   
+  );
+}
+
+export default App;
